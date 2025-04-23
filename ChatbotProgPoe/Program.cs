f05facbd738c02227a98a384efb2a2df2081566f
Ethanos7405! <st10440914@vcconnect.edu.za>
2025-04-23 09:22:06 +0200

    Updated my code quite a bit. Using switch case instead of if statements and improved the UI slightly.

--- ChatbotProgPoe/Program.cs
+++ ChatbotProgPoe/Program.cs
@@ -16,7 +16,6 @@ namespace ChatbotProgPoe
         
         static void Main(string[] args)
         {
-            //string asciiArt = " ___________________\r\n | _______________ |\r\n | |XXXXXXXXXXXXX| |\r\n | |XXXXXXXXXXXXX| |\r\n | |XXXXXXXXXXXXX| |\r\n | |XXXXXXXXXXXXX| |\r\n | |XXXXXXXXXXXXX| |\r\n |_________________|\r\n     _[_______]_\r\n ___[___________]___\r\n|         [_____] []|__\r\n|         [_____] []|  \\__\r\nL___________________J     \\ \\___\\/\r\n ___________________      /\\\r\n/###################\\    (__)";
             string asciiArt = "  ____      _                                        _ _         \r\n / ___|   _| |__   ___ _ __ ___  ___  ___ _   _ _ __(_) |_ _   _ \r\n| |  | | | | '_ \\ / _ \\ '__/ __|/ _ \\/ __| | | | '__| | __| | | |\r\n| |__| |_| | |_) |  __/ |  \\__ \\  __/ (__| |_| | |  | | |_| |_| |\r\n \\____\\__, |_.__/ \\___|_|  |___/\\___|\\___|\\__,_|_|  |_|\\__|\\__, |\r\n  ____|___/        _   ____        _                       |___/ \r\n / ___| |__   __ _| |_| __ )  ___ | |_                           \r\n| |   | '_ \\ / _` | __|  _ \\ / _ \\| __|                          \r\n| |___| | | | (_| | |_| |_) | (_) | |_                           \r\n \\____|_| |_|\\__,_|\\__|____/ \\___/ \\__|                          ";
             string errorMessage = "Sorry, I didn't understand that. Can you please rephrase your question?";
             string userInput;
@@ -26,11 +25,10 @@ namespace ChatbotProgPoe
             //Sound greeting
             try
             {
-                using (SoundPlayer player = new SoundPlayer("Assets/Test.wav"))
+                using (SoundPlayer player = new SoundPlayer("Assets/Greeting.wav"))
                 {
                     player.Load();
                     player.Play();
-                    //player.PlaySync();
                 }
 
             }
@@ -68,7 +66,7 @@ namespace ChatbotProgPoe
             while (runApp == true)
             {
                 textDelay("\n\n---------------------------------------------------------");
-                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
                 textDelay("\nAsk me a Question or type 'exit' to exit: ");
                 Console.ResetColor();
 
@@ -76,99 +74,7 @@ namespace ChatbotProgPoe
                 userInput = Regex.Replace(input, @"[^\w\s]", "");
 
 
-                //Add responses for different user inputs
-
-                /*
-                //general questions
-                if (userInput == "how are you" ||
-                    userInput == "how are you doing" ||
-                    userInput == "whats up" ||
-                    userInput == "are you okay")
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    textDelay("I'm doing great, thank you for asking! Ready to talk cyber safety");
-                    Console.ResetColor();
-                }
-                else if (userInput == "what can i ask you about")
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    textDelay("You can ask me about password safety, phishing, safe browsing and other topics related to cybersecurity.");
-                    Console.ResetColor();
-                }
-
-                //Password Safety
-                else if (userInput == "how do i make a strong password" ||
-                         userInput == "whats a secure password" ||
-                         userInput == "tell me about passwords" ||
-                         userInput == "how to keep my password safe" ||
-                         userInput == "password tips" ||
-                         userInput == "how do i create a good password" ||
-                         userInput == "password help")
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    textDelay("At least 12 characters long but 14 or more is better. A combination of uppercase letters, lowercase letters, numbers, and symbols. Not a word that can be found in a dictionary or the name of a person, character, product, or organization.");
-                    Console.ResetColor();
-                }
-                else if(userInput == "why should i use a password manager")
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    textDelay("Password managers can help you create and store strong, unique passwords for all your accounts, making it easier to manage your online security.");
-                    Console.ResetColor();
-                }
-                else if(userInput == "how often should i change my password")
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    textDelay("It's a good practice to change your passwords every 3-6 months, especially for sensitive accounts. If you suspect a breach, change it immediately.");
-                    Console.ResetColor();
-                }
-                else if (userInput == "how do I remember my passwords")
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    textDelay("you can use a password manager to store them securely or create a memorable phrase that incorporates elements of your password.");
-                    Console.ResetColor();
-                }
-                else if(userInput == "what is two factor authentication" ||
-                        userInput == "tell me about two factor authentication" ||
-                        userInput == "2fa")
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    textDelay("Two-factor authentication (2FA) adds an extra layer of security by requiring not only a password and username but also something that only the user has on them, such as a physical token or a mobile phone.");
-                    Console.ResetColor();
-                }
-                else if (userInput == "how do i enable two factor authentication")
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    textDelay("You can usually enable 2FA in the security settings of your online accounts. Look for options like 'Enable Two-Factor Authentication' or 'Set Up Two-Step Verification'.");
-                    Console.ResetColor();
-                }
-
-                //Phishing
-                else if (userInput == "what is phishing")
-                {
-                    textDelay("Phishing is a type of cyber attack where attackers impersonate legitimate organizations to trick individuals into providing sensitive information, such as passwords or credit card numbers. This is often done through emails, messages, or fake websites.");
-                }
-                else if (userInput == "how do i spot a fake email" ||
-                         userInput == "how can i recognize a phishing email")
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    textDelay("They might contain bad spelling or grammar, come from an unusual email address, or feature imagery or design that feels 'off'. But scams are getting smarter and some even fool the experts. Criminals are increasingly using QR codes within phishing emails to trick users into visiting scam websites.");
-                    Console.ResetColor();
-                }
-
-                //Exit Program
-                else if (userInput == "exit")
-                {
-                    textDelay("Exiting application. Goodbye!!");
-                    runApp = false;
-                }
-
-                //Error Message
-                else
-                {
-                    Console.WriteLine(errorMessage);
-                }
-                */
-
+                //Add responses for different user inputs              
 
                 switch (userInput)
                 {
@@ -238,12 +144,12 @@ namespace ChatbotProgPoe
                     case "what should i do if i suspect a fake email":
                     case "what do i do if i suspect a phishing email":
                         Console.ForegroundColor = ConsoleColor.Yellow;
-                        textDelay($"If you suspect an email is phishing, do not click on any links or download attachments. Report it to your email provider and delete it.");
+                        textDelay("If you suspect an email is phishing, do not click on any links or download attachments. Report it to your email provider and delete it.");
                         Console.ResetColor();
                         break;
                     case "can phishing happen on social media":
                         Console.ForegroundColor = ConsoleColor.Yellow;
-                        textDelay("Yes, phishing can occur on social media. Attackers may send direct messages or post fake offers or links that direct you to phishing websites. Be cautious when clicking on links from unknown users or profiles, and never share personal information on unverified platforms.");
+                        textDelay($"Yes {name}, phishing can occur on social media. Attackers may send direct messages or post fake offers or links that direct you to phishing websites. Be cautious when clicking on links from unknown users or profiles, and never share personal information on unverified platforms.");
                         Console.ResetColor();
                         break;
                     
@@ -254,25 +160,46 @@ namespace ChatbotProgPoe
                     case "what makes a website secure":
                     case "how do i know if a website is secure":
                         Console.ForegroundColor = ConsoleColor.Yellow;
-                        textDelay("Look for HTTPS in the URL, a padlock icon in the address bar, and check for reviews or ratings of the site. Avoid visiting sites that have poor design, contain suspicious pop-ups, or ask for unnecessary personal information.");
+                        textDelay($"It's very easy to tell if a website is safe {name}. Look for HTTPS in the URL, a padlock icon in the address bar, and check for reviews or ratings of the site. Avoid visiting sites that have poor design, contain suspicious pop-ups, or ask for unnecessary personal information.");
                         Console.ResetColor();
                         break;
                     case "how can i protect myself from malware":
                         Console.ForegroundColor = ConsoleColor.Yellow;
-                        textDelay("Use a reputable antivirus program, keep your browser and operating system up to date, and avoid downloading files or clicking links from untrusted sources. Consider using browser extensions that block malicious ads and websites.");
+                        textDelay($"Use a reputable antivirus program. There are also a couple of free options {name}. Keep your browser and operating system up to date, and avoid downloading files or clicking links from untrusted sources. Consider using browser extensions that block malicious ads and websites.");
                         Console.ResetColor();
                         break;
                     case "what is incognite mode":
                     case "is incognito mode safe":
                     case "inognite mode":
                         Console.ForegroundColor = ConsoleColor.Yellow;
-                        textDelay("Incognito (or private) mode hides your browsing history from others on your device—but it doesn’t hide it from your internet provider, employer, or the websites you visit.");
+                        textDelay($"Incognite mode hides your browsing history from the other people on your device. However {name}, it doesn’t hide it from your internet provider, employer, or the websites you visit.");
                         Console.ResetColor();
                         break;
                     case "what are cookies":
                     case "cookies":
                         Console.ForegroundColor = ConsoleColor.Yellow;
-                        textDelay("Cookies store data about your browsing habits. While some improve your experience, others track you. Blocking third-party cookies enhances privacy.");
+                        textDelay($"Cookies store data about your browsing habits {name}. Some cookies improve the user's experience but there are others that will track you. Blocking third-party cookies enhances privacy.");
+                        Console.ResetColor();
+                        break;
+                    case "is public wifi safe":
+                    case "how to stay safe on public wifi":
+                    case "what if i have to use public wifi":
+                    case "can hackers see what i do on public wifi":
+                    case "how do i protect my data on public wifi":
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        textDelay($"Public Wi-Fi is risky {name}. You should avoid banking or shopping. Use a VPN, disable sharing, and never enter passwords or sensitive info unless you're on a secured network.");
+                        Console.ResetColor();
+                        break;
+                    case "how can i stay safe on social media":
+                    case "should i post my location online":
+                    case "what if someone is impersonating me":
+                    case "how to make my account private":
+                    case "is it safe to accept random friend requests":
+                    case "should i post my birthday online":
+                    case "how to stop people from seeing my info":
+                    case "how do i report abuse on social media":
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        textDelay("You should keep your account private {name}. Make sure you don’t share personal info like address or birthday, avoid strangers, and report anything suspicious. Make sure you review your privacy settings often aswell.");
                         Console.ResetColor();
                         break;
 
